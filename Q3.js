1dbcf6ccdf86de6714fd4d2302feff5ccc668b59
GUYROSILLO <guyrosillo@gmail.com>
2017-11-08 15:04:06 -0800

    Initial commit

---
+++ Q3.js
@@ -0,0 +1,8 @@
+// Question 3
+
+// Inside of console.log, write a Boolean expression that tests
+// whether two numbers are NOT equal, and whose answer evaluates to true.
+
+const a = 6;
+const b = 8;
+console.log(a !== b);
